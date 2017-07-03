33b7b2238e47bc92c0b2d6a1bd67de5b4c11b9d9
bepid <IFCE@bepids-imac-8.local>
2017-07-03 16:08:11 -0300

    Initial Commit

---
+++ VeetoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VeetoApp
+//
+//  Created by IFCE on 03/07/17.
+//  Copyright © 2017 IFCE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

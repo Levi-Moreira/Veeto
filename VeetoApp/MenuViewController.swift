a13fbf960a4bce4e04b0052b98a4c8de44eab40d
IFCE <IFCE@bpid.local>
2017-07-14 15:53:43 -0300

    Add settings

--- VeetoApp/MenuViewController.swift
+++ VeetoApp/MenuViewController.swift
@@ -11,9 +11,16 @@ import SCLAlertView
 
 class MenuViewController: UIViewController, UINavigationControllerDelegate {
 
+    let preferences = UserDefaults.standard
+    
+    let soundSettings = "soundSettings"
+    let timeSettings = "timeSettings"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,12 +37,50 @@ class MenuViewController: UIViewController, UINavigationControllerDelegate {
             showCircularIcon: true
         )
         let alertView = SCLAlertView(appearance: appearance)
-        let alertViewIcon = UIImage(named: "settings") //Replace the IconImage text with the image name
-//        alertView.showInfo("Custom icon", subTitle: "This is a nice alert with a custom icon you choose", circleIconImage: alertViewIcon)
-        let view = UIView(frame: CGRect(x: 0, y: 0,width: 500,height: 500))
-        view.backgroundColor = .red
+        let alertViewIcon = UIImage(named: "settings") //Replace the IconImage text with the 
         
-        alertView.customSubview = view
+        
+        let subview = UIView(frame: CGRect(x: 0, y: 0,width: 500,height: 550))
+        
+        
+        let soundLabel = UILabel(frame: CGRect(x: 0,y: 15, width: 100,height: 25))
+        
+        soundLabel.text = "Sound"
+        soundLabel.textAlignment = NSTextAlignment.center
+        subview.addSubview(soundLabel)
+        
+        let switchView = UISwitch(frame: CGRect(x: 130, y: 15, width: 0, height: 25))
+        
+    
+        switchView.addTarget(self, action: #selector(didChangeSoundSettings), for: .valueChanged)
+        
+        if(preferences.bool(forKey: soundSettings)){
+            switchView.isOn = true
+        }else{
+            switchView.isOn = false
+        }
+        
+        subview.addSubview(switchView)
+        
+        let timeLabel = UILabel(frame: CGRect(x: 0,y: 77, width: 100,height: 25))
+        timeLabel.text = "Time"
+        timeLabel.textAlignment = NSTextAlignment.center
+        subview.addSubview(timeLabel)
+        
+        
+        let items = ["60", "90", "120"]
+        let segmented = UISegmentedControl(items:items)
+        segmented.frame = CGRect(x: 100,y: 72, width: 100,height: 30)
+        
+        
+        segmented.addTarget(self, action: #selector(didChangeTimeSettings), for: .valueChanged)
+    
+        segmented.selectedSegmentIndex = preferences.integer(forKey: timeSettings)
+        
+        subview.addSubview(segmented)
+        
+        
+        alertView.customSubview = subview
         
         
         alertView.showTitle(
@@ -48,8 +93,22 @@ class MenuViewController: UIViewController, UINavigationControllerDelegate {
             colorTextButton: 0xFFFFFF,
             circleIconImage: alertViewIcon
         )
+    
+    }
+    
+    
+    func didChangeSoundSettings(sender: UISwitch!){
         
+        if(sender.isOn){
+            preferences.set(true, forKey: soundSettings)
+        }else{
+            preferences.set(false, forKey: soundSettings)
+        }
         
+    }
+    
+    func didChangeTimeSettings(sender: UISegmentedControl!){
+        preferences.set(sender.selectedSegmentIndex, forKey: timeSettings)
     }
 
 }
